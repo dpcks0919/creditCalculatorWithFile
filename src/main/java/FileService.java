0a0f682b108b3ca6aa57c1ffc739358e456a12df
dpcks0919 <21600120@handong.edu>
2021-06-29 13:45:17 +0900

    fileRead 방법 추가

--- src/main/java/FileService.java
+++ src/main/java/FileService.java
@@ -12,10 +12,20 @@ public class FileService {
         ArrayList<Person> list = new ArrayList<>();
 
         try{
-            // fileUtils 사용
-            List<String> lines = FileUtils.readLines(new File("data.txt"));
-            for(int i =0 ; i < lines.size(); i++){
-                StringTokenizer st = new StringTokenizer(lines.get(i) , "/");
+
+            // bufferedReader, FileReader 사용
+            //파일 객체 생성
+            File file = new File("data.txt");
+            //입력 스트림 생성
+            FileReader filereader = new FileReader(file);
+            //입력 버퍼 생성
+            BufferedReader bufReader = new BufferedReader(filereader);
+
+            int i = 0;
+            String line = "";
+            while((line = bufReader.readLine()) != null){  //.readLine()은 끝에 개행문자를 읽지 않는다.
+
+                StringTokenizer st = new StringTokenizer(line , "/");
 
                 String name = st.nextToken().trim();
                 int korScore = Integer.parseInt(st.nextToken().trim());
@@ -25,9 +35,27 @@ public class FileService {
                 String regDate = st.nextToken().trim();
 
                 list.add(new Person(i, name, korScore, engScore, mathScore, grade, regDate));
-                //createData(name, korScore, engScore, mathScore, grade, regDate);
+                i++;
             }
+            bufReader.close();
+            // bufferedReader 사용
+
             // fileUtils 사용
+//            List<String> lines = FileUtils.readLines(new File("data.txt"));
+//            for(int i =0 ; i < lines.size(); i++){
+//                StringTokenizer st = new StringTokenizer(lines.get(i) , "/");
+//
+//                String name = st.nextToken().trim();
+//                int korScore = Integer.parseInt(st.nextToken().trim());
+//                int engScore = Integer.parseInt(st.nextToken().trim());
+//                int mathScore = Integer.parseInt(st.nextToken().trim());
+//                String grade = Person.calculateGrade(korScore, engScore, mathScore);
+//                String regDate = st.nextToken().trim();
+//
+//                list.add(new Person(i, name, korScore, engScore, mathScore, grade, regDate));
+//            }
+            // fileUtils 사용
+
         }catch (FileNotFoundException e) {
             System.out.println("data.txt 파일이 존재하지 않습니다.");
         }catch(IOException e){
